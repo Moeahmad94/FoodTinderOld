eeb9167291bfe70454aaf09de8925a544bea82f1
pinak28 <pinak28@gmail.com>
2015-01-17 02:18:20 -0500

    Outlets and Actions added

---
+++ FoodTinder/TinderViewController.swift
@@ -0,0 +1,29 @@
+//
+//  TinderViewController.swift
+//  FoodTinder
+//
+//  Created by Pinak Shikhare on 1/17/15.
+//  Copyright (c) 2015 Mohammad Ahmad. All rights reserved.
+//
+
+import UIKit
+
+class TinderViewController: UIViewController {
+    
+    
+	
+    @IBAction func reject(sender: AnyObject) {
+        
+        
+    }
+	
+    @IBAction func like(sender: AnyObject) {
+        
+        
+    }
+	
+    @IBOutlet weak var imageShown: UIImageView!
+    @IBOutlet weak var textField: UITextView!
+	
+	
+}
